42ce446a5e979884179911f6a38c8e765ef22120
jaac <38704271+jonasaacampos@users.noreply.github.com>
2022-08-18 11:35:43 -0300

    MVC Leilao
    finalizacao da classe, controller e view do leilao

--- MvcAuction/Models/Leilao.cs
+++ MvcAuction/Models/Leilao.cs
@@ -7,5 +7,17 @@ namespace MvcAuction.Models
 {
     public class Leilao
     {
+        public long Id { get; set; }
+        public string Descricao { get; set; }
+        public string ImageUrl { get; set; }
+
+        public DateTime TimeStarts { get; set; }
+        public DateTime TimeEnds { get; set; }
+
+        public decimal PrecoInicial { get; set; }
+        public decimal? PrecoAtual { get; set; }
+
+
+
     }
 }

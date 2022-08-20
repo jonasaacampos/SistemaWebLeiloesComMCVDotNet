a5f91427f24c3896f9694da629641484100cc3c3
jaac <38704271+jonasaacampos@users.noreply.github.com>
2022-08-20 10:03:56 -0300

    melhoria performance
    controle asyc cash leiloes e possibilidade de raspagem futura

---
+++ MvcAuction/Controllers/BuscaController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcAuction.Controllers
+{
+    public class BuscaController : AsyncController
+    {
+        public async Task<ActionResult> Leilao(string keyword)
+        {
+            var leilao = await Task.Run<IEnumerable<Models.Leilao>>(
+                () =>
+                {
+                    var database = new Models.LeiloesDataContext();
+                    return database.Leiloes.Where(x => x.NomeLeilao.Contains(keyword)).ToArray();
+                });
+
+            return Json(leilao, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
